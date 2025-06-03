4f5a9beaa21dc60ab21d086e3c3aeea881ac5ecd
Enoch <yinuozhou027@berkeley.edu>
2025-06-03 11:32:22 -0700

    fix: sign up ui update update ui in sign up pane with flex box and other improvements

--- chrome/content/zotero/DeepTutor.jsx
+++ chrome/content/zotero/DeepTutor.jsx
@@ -1003,7 +1003,7 @@ var DeepTutor = class DeepTutor extends React.Component {
 								display: 'flex',
 								justifyContent: 'space-between',
 								alignItems: 'center',
-								marginBottom: '1.25rem',
+								marginBottom: '2rem',
 								minHeight: '4rem',
 								padding: '0 1rem',
 							}}>
@@ -1307,7 +1307,7 @@ var DeepTutor = class DeepTutor extends React.Component {
 					}}>
 						<div style={{
 							position: 'relative',
-							width: '80%',
+							width: '90%',
 							maxWidth: '26.875rem',
 							maxHeight: '80%',
 							background: '#FFFFFF',
@@ -1319,7 +1319,7 @@ var DeepTutor = class DeepTutor extends React.Component {
 								display: 'flex',
 								justifyContent: 'space-between',
 								alignItems: 'center',
-								marginBottom: '1.25rem',
+								marginBottom: '2rem',
 								minHeight: '4rem',
 								padding: '0 1rem',
 							}}>
@@ -1333,8 +1333,8 @@ var DeepTutor = class DeepTutor extends React.Component {
 									backgroundClip: 'text',
 									color: '#0687E5',
 									fontWeight: 700,
-									fontSize: '1rem',
-									lineHeight: '100%',
+									fontSize: '1.5rem',
+									lineHeight: '1.2',
 									letterSpacing: '0%',
 								}}>
 									Sign up
