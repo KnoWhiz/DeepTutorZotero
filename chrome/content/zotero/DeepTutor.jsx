24c832c323c8e389050c5327ab1d8eec6a7d1b4b
Enoch <yinuozhou027@berkeley.edu>
2025-06-12 18:23:28 -0700

    fix: remove last commit remove changes in last commit

--- chrome/content/zotero/DeepTutor.jsx
+++ chrome/content/zotero/DeepTutor.jsx
@@ -696,7 +696,7 @@ var DeepTutor = class DeepTutor extends React.Component {
 				if (!this.state.isLoadingSessions) {
 					await this.loadSession();
 				}
-				this.switchPane(this.getSessionHistoryPaneOrNoSession());
+				// this.switchPane(this.getSessionHistoryPaneOrNoSession());
 			} else {
 				// User signed out, clear data and show welcome
 				this.setState({
@@ -1166,7 +1166,6 @@ var DeepTutor = class DeepTutor extends React.Component {
 							onSignInSignUp={() => this.toggleSignUpPopup()}
 							onSignInSuccess={() => {
 								this.loadSession();
-								this.switchPane(this.getSessionHistoryPaneOrNoSession());
 								this.toggleSignInPopup();
 							}}
 						/>}
